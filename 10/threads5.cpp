bac0dacb5894c3cbbf9b9d2da4049d5b714d08ce
MikeShah <mshah.475@gmail.com>
2021-03-30 10:51:11 -0400

    added comments

--- 10/threads5.cpp
+++ 10/threads5.cpp
@@ -10,6 +10,12 @@ int square(int x) {
 
 int main() {
     auto a = async(&square, 10);
+    cout << "program is continuing at this point\n";
+    // some other work here
+    // ...
+    // ...
+    // But now I really need to make sure
+    // I have the value from 'a', so I call .get().
     int v = a.get();
 
     cout << "The thread returned " << v << endl;
