2d0f2b15088cf8f9a8e027b1dba7728b2b0e66f3
Mike <mike@system76-pc.localdomain>
2024-08-30 15:39:11 -0400

    compile time

---
+++ dlang/2/compile_time.d
@@ -0,0 +1,14 @@
+// @file compile_time.d
+// Note: Just 'compile' this file, do not execute it.
+//       The 'pragma' willl then 'print' at compile-time the values
+//       when that line is parsed.
+//       Again -- the program is not running, only the compiler, evaluating
+//       the function call if all values are known at compile-time.
+import std.algorithm;
+void main(){
+
+	enum values = [7,12,15,17,14];
+	values.sort;
+	
+	pragma(msg,values);
+}
