6baa64e3e5b8dd4fcca1fc35639ac0da9a9e28c5
Mike <mike@system76-pc.localdomain>
2024-09-17 19:01:42 -0400

    examples

---
+++ dlang/8_sdl/sdl_dub_examples/03_sdl_event_loop/source/app.d
@@ -0,0 +1,46 @@
+/// @file: sdl_dub_examples/03_sdl_event_loop/app.d
+/// 
+import std.stdio;
+/// Run with: 'dub' 'dub run'
+/// On occasion, 'dub clean' followed by 'dub build' to do a rebuild.
+/// 'dub test' will run your tests.
+///
+/// 'dub lint' can also be useful for style checking and best practices
+/// usually using the dscanner tool. 
+
+// Loads SDL Libraries
+// NOTE: sdl_asbstraction currently runs 'SDL_Init' before main(), and
+//       will run 'SDL_Quit' when the program terminates.
+import sdl_abstraction;
+
+// Third-party library which provides
+// declarations of functions and structs in SDL
+import bindbc.sdl;
+
+// Entry point to program
+void main()
+{
+		SDL_Window* window = SDL_CreateWindow("Dlang SDL Window",
+						0,0, 640,480, SDL_WINDOW_SHOWN);
+
+		bool gameIsRunning=true;
+		while(gameIsRunning){
+				// Store an SDL Event
+				SDL_Event event;
+				// SDL_Pollevent pops off any events that are queued up.
+				// This loop will thus run until all events are handled.
+				while(SDL_PollEvent(&event)){
+						if(event.type == SDL_QUIT){
+								writeln("Exit event triggered");
+								gameIsRunning= false;
+						}
+					writeln("event loop");
+				}
+			
+				writeln("main game loop");
+				// After we handle 'input' we can run the rest of the game here.
+
+				// Update();
+				// Render();
+		}
+} // end main()
