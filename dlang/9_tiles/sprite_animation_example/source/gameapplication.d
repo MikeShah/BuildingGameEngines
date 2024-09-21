806ab282cc22eec8af37acac30e69c8af657e005
Mike <mike@system76-pc.localdomain>
2024-09-21 18:50:13 -0400

    examples for tiling

---
+++ dlang/9_tiles/sprite_animation_example/source/gameapplication.d
@@ -0,0 +1,85 @@
+module gameapplication;
+// Import D standard libraries
+import std.stdio;
+import std.string;
+
+// Third-party libraries
+import bindbc.sdl;
+
+// Import our SDL Abstraction
+import sdl_abstraction;
+import sprite;
+
+struct GameApplication{
+		SDL_Window* mWindow = null;
+		SDL_Renderer* mRenderer = null;
+		bool mGameIsRunning = true;
+
+		// Game Data
+		Sprite mySprite;
+
+		// Constructor
+		this(string title){
+				// Create an SDL window
+				mWindow = SDL_CreateWindow(title.toStringz, SDL_WINDOWPOS_UNDEFINED, 
+								SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+
+				// Create a hardware accelerated mRenderer
+				mRenderer = SDL_CreateRenderer(mWindow,-1,SDL_RENDERER_ACCELERATED);
+				// Load the bitmap surface
+				mySprite = Sprite(mRenderer,"./assets/images/test.bmp");
+		}
+
+		// Destructor
+		~this(){
+				// Destroy our renderer
+				SDL_DestroyRenderer(mRenderer);
+				// Destroy our window
+				SDL_DestroyWindow(mWindow);
+		}
+
+		// Handle input
+		void Input(){
+				SDL_Event event;
+				// Start our event loop
+				while(SDL_PollEvent(&event)){
+						// Handle each specific event
+						if(event.type == SDL_QUIT){
+								mGameIsRunning= false;
+						}
+				}
+				mySprite.Input();
+		}
+
+		void Update(){
+				mySprite.Update();
+		}
+
+		void Render(){
+				// Set the render draw color 
+				SDL_SetRenderDrawColor(mRenderer,100,190,255,SDL_ALPHA_OPAQUE);
+				// Clear the renderer each time we render
+				SDL_RenderClear(mRenderer);
+
+				mySprite.Render(mRenderer);
+
+				// Final step is to present what we have copied into
+				// video memory
+				SDL_RenderPresent(mRenderer);
+		}
+
+		// Advance world one frame at a time
+		void AdvanceFrame(){
+				Input();
+				Update();
+				Render();
+				SDL_Delay(200);
+		}
+
+		void RunLoop(){
+				// Main application loop
+				while(mGameIsRunning){
+						AdvanceFrame();	
+				}
+		}
+}
