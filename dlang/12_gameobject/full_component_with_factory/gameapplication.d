1051f3d4c9edb0704144ec9b89453efed1b2f8ea
Mike <mike@system76-pc.localdomain>
2024-10-02 20:12:58 -0400

    Added variadic function factory

--- dlang/12_gameobject/full_component_with_factory/gameapplication.d
+++ dlang/12_gameobject/full_component_with_factory/gameapplication.d
@@ -26,6 +26,30 @@ GameObject CreateTextureColliderGameObject(string name){
 		return go;
 }
 
+// Meta-programming to generate factories for creating game objects
+// See: https://dlang.org/articles/variadic-function-templates.html
+// CAUTION: Each new ordering of components will instantiate a new type.
+// 					I'd thus recommend 'sorting' the variadic arguments. That takes
+//          a little bit more work, and I'll leave as an exercise until someone asks..
+GameObject GameObjectFactory(T...)(string name){
+	// Create our game object
+	GameObject go = GameObject(name);
+	// Static foreach loop will be 'unrolled' with
+	// each 'if' condition for what is true.
+	// This could also handle the case where we repeat component types as well if our
+	// game object supports multiple components of the same type.
+	static foreach(component ; T){
+		static if(component == ComponentType.TEXTURE){
+			go.AddComponent!(component)(new ComponentTexture(go.GetID()));
+		}else if(component == ComponentType.COLLISION){
+			go.AddComponent!(component)(new ComponentCollision(go.GetID()));
+		}else{
+			assert(0, "Did not find right component");
+		}
+	}
+	return go;
+}
+
 struct GameApplication{
 
 	this(int objects){
@@ -33,9 +57,14 @@ struct GameApplication{
 			// Create some different game objects from a factory
 			if(i%2==0){
 				mGameObjects ~= CreateTextureColliderGameObject("GameObject"~i.to!string);
-			}else{
+			}else if(i==4){
+				mGameObjects ~= GameObjectFactory!(ComponentType.COLLISION,ComponentType.TEXTURE)("GameObject"~i.to!string);
+			}
+			else{
 				mGameObjects ~= CreateTextureGameObject("GameObject"~i.to!string);
 			}
+		
+
 		}
 	}
 
