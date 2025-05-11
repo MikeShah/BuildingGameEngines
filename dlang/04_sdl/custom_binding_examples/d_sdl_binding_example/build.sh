6594902d5c2265fa8c1ff93998af08fcdd9bae4e
Mike <mike@system76-pc.localdomain>
2025-05-11 17:26:01 -0400

    adding build script

---
+++ dlang/04_sdl/custom_binding_examples/d_sdl_binding_example/build.sh
@@ -0,0 +1,11 @@
+# Simple build script
+# Remember to 'link in SDL'
+#
+# Tools like 'pkg-config' can also help
+# e.g.
+# pkg-config --libs sdl3
+#
+# Usage: dmd -g source/*.d -L`pkg-config --libs sdl3` -of=prog
+#
+# Linux
+dmd -g *.d -L-lSDL3 -of=prog
