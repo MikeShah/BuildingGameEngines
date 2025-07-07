8c27f33516513ccd659642975d27420cf2c302f2
MikeShah <mshah.475@gmail.com>
2025-07-06 20:45:49 -0400

    xpixmap.d

---
+++ dlang/experiments/image/xpixmap.d
@@ -0,0 +1,5 @@
+// @file: xpixmap.d
+//
+// This is a small compact image format. It can be used for pixel data, and often
+// is used for things like glyphs or icons.
+// https://en.wikipedia.org/wiki/X_PixMap
