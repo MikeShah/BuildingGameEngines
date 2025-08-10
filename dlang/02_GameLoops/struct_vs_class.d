ba05e538f39b8a26148f8e52719fb82759f852db
Mike <mike@system76-pc.localdomain>
2025-08-09 21:05:29 -0400

    more notes on struct vs class

--- dlang/02_GameLoops/struct_vs_class.d
+++ dlang/02_GameLoops/struct_vs_class.d
@@ -1,13 +1,18 @@
 // @file: struct_vs_class.d
  
-// - Value Type
-// - No inheritance
-// - Default is 'stack' allocated, but can also be
-//   allocatd with 'new'
-// - No default constructor needed, but any constructors
-//   defined need 1 or more arguments
-// - Deterministic destruction when allocated on the stack
-//   i.e. RAII (Resource Allocation is Initialization
+/+
+  - Basic Information
+	- Value Type
+	- No inheritance
+	- Default is 'stack' allocated, but can also be
+	  allocatd with 'new'
+	- No default constructor needed, but any constructors
+	  defined need 1 or more arguments
+	- Deterministic destruction when allocated on the stack
+	  i.e. RAII (Resource Allocation is Initialization
+  - More Advanced Information
+    - 
++/
 struct MyStruct{
 	int mField1;
 	int mField2;
@@ -20,10 +25,23 @@ struct MyStruct{
 	}
 }
 
-// - Reference Type
-// - Must be allocated on heap with 'new'
-// - Allows for Single inheritance
-// - Can have inheritance from multiple interfaces
+/+
+ - Basic Information
+   - Reference Type
+   - Must be allocated on heap with 'new'
+   - Allows for Single inheritance
+   - Can have inheritance from multiple interfaces
+ - More Advanced Information
+   - There is no deterministic destruction. While there is
+     a destructor, we cannot know when the garbage collector will 
+     reclaim the memory.
+   - Every class inherits otherwise from 'Object'.
+     This is sort of similar to Java, and you can find object.d.
+	 Some D Developers will write their own 'object.d' to override the
+     behavior -- this can be useful when targetting web platforms for example. 
+   - The D Language Runtime is needed to support classes
+     you can explore 'TypeInfo' for more information.
++/
 class MyClass{
 	this(){}
 	~this(){}
