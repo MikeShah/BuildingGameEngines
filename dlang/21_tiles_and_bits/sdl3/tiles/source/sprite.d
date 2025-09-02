3e918ac149bdbc4f9095f2ab82980616f468b5df
MikeShah <mshah.475@gmail.com>
2025-09-02 09:51:03 -0400

    sdl3 upgrade

--- dlang/21_tiles_and_bits/sdl3/tiles/source/sprite.d
+++ dlang/21_tiles_and_bits/sdl3/tiles/source/sprite.d
@@ -9,49 +9,49 @@ enum STATE{IDLE, WALK};
 /// Sprite that holds a texture and position
 struct Sprite{
 
-        int mXPos=50;
-        int mYPos=50;
-		SDL_Rect mRectangle;
-		SDL_Texture* mTexture;
-        int mFrame;
+  int mXPos=50;
+  int mYPos=50;
+  SDL_FRect mRectangle;
+  SDL_Texture* mTexture;
+  int mFrame;
 
-        STATE mState;
+  STATE mState;
 
-		this(SDL_Renderer* renderer, string filepath){
-			// Load the bitmap surface
-			SDL_Surface* myTestImage   = SDL_LoadBMP(filepath.ptr);
-			// Create a texture from the surface
-			mTexture = SDL_CreateTextureFromSurface(renderer,myTestImage);
-			// Done with the bitmap surface pixels after we create the texture, we have
-			// effectively updated memory to GPU texture.
-			SDL_FreeSurface(myTestImage);
+  this(SDL_Renderer* renderer, string filepath){
+    // Load the bitmap surface
+    SDL_Surface* myTestImage   = SDL_LoadBMP(filepath.ptr);
+    // Create a texture from the surface
+    mTexture = SDL_CreateTextureFromSurface(renderer,myTestImage);
+    // Done with the bitmap surface pixels after we create the texture, we have
+    // effectively updated memory to GPU texture.
+    SDL_DestroySurface(myTestImage);
 
-			// Rectangle is where we will represent the shape
-			mRectangle.x = mXPos;
-			mRectangle.y = mYPos;
-			mRectangle.w = 64;
-			mRectangle.h = 64;
-		}
+    // Rectangle is where we will represent the shape
+    mRectangle.x = mXPos;
+    mRectangle.y = mYPos;
+    mRectangle.w = 64;
+    mRectangle.h = 64;
+  }
 
-		void Render(SDL_Renderer* renderer){
+  void Render(SDL_Renderer* renderer){
 
-			SDL_Rect selection;
-			selection.x = 64*mFrame;
-			selection.y = 0;
-			selection.w = 64;
-			selection.h = 64;
+    SDL_FRect selection;
+    selection.x = 64*mFrame;
+    selection.y = 0;
+    selection.w = 64;
+    selection.h = 64;
 
-			mRectangle.x = mXPos;
-			mRectangle.y = mYPos;
+    mRectangle.x = mXPos;
+    mRectangle.y = mYPos;
 
-    	    SDL_RenderCopy(renderer,mTexture,&selection,&mRectangle);
+    SDL_RenderTexture(renderer,mTexture,&selection,&mRectangle);
 
-            if(mState == STATE.WALK){
-			    mFrame++;
-                if(mFrame > 3){
-                    mFrame =0;
-                }
-            }
-		}
+    if(mState == STATE.WALK){
+      mFrame++;
+      if(mFrame > 3){
+        mFrame =0;
+      }
+    }
+  }
 }
 
