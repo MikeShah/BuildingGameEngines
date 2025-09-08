3c84952829d0907ec73c6c729feb6ce06d3d0820
Mike <mike@system76-pc.localdomain>
2025-09-08 13:56:09 -0400

    added another example

--- dlang/03_d_language/02_systems_programming_conepts/pointers2.d
+++ dlang/03_d_language/02_systems_programming_conepts/pointers2.d
@@ -3,12 +3,22 @@
 import std.stdio;
 
 void main(){
-    int   x = 7;
-    auto px = &x;
-		auto y = 8;
-		writeln("y is: ",typeid(y));
-    
-    writeln(x);
-    writeln(*px); 
-    writeln(typeid(px));
+
+	int[] arr = [1,2,3];
+	arr ~= 5;
+	writeln("&arr      : ",&arr);
+	writeln("arr.ptr   : ",arr.ptr);
+	writeln("arr.length: ",arr.length);
+
+	writeln();
+
+	// Expanding this example a bit more, 
+	// observe what happens to an array's .ptr
+	// address for a 'stack-allocated' pointer.
+	int[3] arr2 = [1,2,3];
+	arr2 ~= 4;
+	writeln("&arr      : ",&arr2);
+	writeln("arr.ptr   : ",arr2.ptr);
+	writeln("arr.length: ",arr2.length);
+
 }
