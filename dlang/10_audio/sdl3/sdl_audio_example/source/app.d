3a7d01b19468bb187e6ba86068b8451638a9d225
MikeShah <mshah.475@gmail.com>
2026-09-26 16:54:33 -0400

    added looping

--- dlang/10_audio/sdl3/sdl_audio_example/source/app.d
+++ dlang/10_audio/sdl3/sdl_audio_example/source/app.d
@@ -8,86 +8,95 @@ import sdl_abstraction;
 
 struct Sprite{
 
-    SDL_Texture* mTexture;
-    SDL_FRect    mRectangle;
+  SDL_Texture* mTexture;
+  SDL_FRect    mRectangle;
 
-    this(SDL_Renderer* renderer, string bitmapFilePath){
-        SDL_Surface* mSurface = SDL_LoadBMP(bitmapFilePath.toStringz);
-        mTexture = SDL_CreateTextureFromSurface(renderer,mSurface);
-        SDL_DestroySurface(mSurface);
-        // Position the rectangle 
-        mRectangle.x = 50;
-        mRectangle.y = 50;
-        mRectangle.w = 100;
-        mRectangle.h = 100;
-    }
+  this(SDL_Renderer* renderer, string bitmapFilePath){
+    SDL_Surface* mSurface = SDL_LoadBMP(bitmapFilePath.toStringz);
+    mTexture = SDL_CreateTextureFromSurface(renderer,mSurface);
+    SDL_DestroySurface(mSurface);
+    // Position the rectangle 
+    mRectangle.x = 50;
+    mRectangle.y = 50;
+    mRectangle.w = 100;
+    mRectangle.h = 100;
+  }
 
-    // Destroy anything 'heap' allocated.
-    // Remember, SDL is a C library, thus heap allocated resources need
-    // to be destroyed
-    ~this(){
-        SDL_DestroyTexture(mTexture);
-    }
+  // Destroy anything 'heap' allocated.
+  // Remember, SDL is a C library, thus heap allocated resources need
+  // to be destroyed
+  ~this(){
+    SDL_DestroyTexture(mTexture);
+  }
 
-    void Input(){}
-    void Update(){}
+  void Input(){}
+  void Update(){}
 
-    void Render(SDL_Renderer* renderer){
-        // Copy a texture (or portion of a texture) to another
-        // portion of video memory (i.e. a 2D grid of texels 
-        // which span the width and height of the window)
-        SDL_RenderTexture(renderer,mTexture,null,&mRectangle);
-    }
+  void Render(SDL_Renderer* renderer){
+    // Copy a texture (or portion of a texture) to another
+    // portion of video memory (i.e. a 2D grid of texels 
+    // which span the width and height of the window)
+    SDL_RenderTexture(renderer,mTexture,null,&mRectangle);
+  }
 }
 
 void main()
 {
 
-	SDL_InitFlags audioSubSystem = SDL_WasInit(SDL_INIT_AUDIO);
-	writeln("Was Audio Subystem initialized? (0 if not, 'audio' if so):", audioSubSystem);
+  SDL_InitFlags audioSubSystem = SDL_WasInit(SDL_INIT_AUDIO);
+  writeln("Was Audio Subystem initialized? (0 if not, 'audio' if so):", audioSubSystem);
 
-    SDL_Window* window = SDL_CreateWindow("Dlang SDL Window",640 ,480, SDL_WINDOW_ALWAYS_ON_TOP);
+  SDL_Window* window = SDL_CreateWindow("Dlang SDL Window",640 ,480, SDL_WINDOW_ALWAYS_ON_TOP);
 
-    // Create a hardware accelerated mRenderer
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, null);
+  // Create a hardware accelerated mRenderer
+  SDL_Renderer* renderer = SDL_CreateRenderer(window, null);
 
-    // Create our sprite
-    Sprite mySprite = Sprite(renderer,"./assets/images/test.bmp");
+  // Create our sprite
+  Sprite mySprite = Sprite(renderer,"./assets/images/test.bmp");
 
-    // Create our Sound
-    Sound mySound = Sound("./assets/sounds/collide.wav");
+  // Create our Sound
+  Sound mySound1 = Sound("./assets/sounds/collide.wav");
+  Sound mySound2 = Sound("./assets/sounds/whoosh.wav");
+  Sound mySound3 = Sound("./assets/sounds/flowlab.wav");
 
-    bool gameIsRunning=true;
-    while(gameIsRunning){
-        // Store an SDL Event
-        SDL_Event event;
-        while(SDL_PollEvent(&event)){
-            if(event.type == SDL_EVENT_QUIT){
-                writeln("Exit event triggered");
-                gameIsRunning= false;
-            }
-            if(event.type == SDL_EVENT_KEY_DOWN){
-				mySound.ResumeSound();
-        		mySound.PlaySound();
-            }
-        }
-
-        // Update our sprite
-        mySprite.Update();
-
-        // Set the render draw color 
-        SDL_SetRenderDrawColor(renderer,100,190,255,SDL_ALPHA_OPAQUE);
-        // Clear the renderer each time we render
-        SDL_RenderClear(renderer);
-
-        mySprite.Render(renderer);
-
-        // Final step is to present what we have copied into
-        // video memory
-        SDL_RenderPresent(renderer);
+  bool gameIsRunning=true;
+  while(gameIsRunning){
+    // Store an SDL Event
+    SDL_Event event;
+    while(SDL_PollEvent(&event)){
+      if(event.type == SDL_EVENT_QUIT){
+        writeln("Exit event triggered");
+        gameIsRunning= false;
+      }
+      if(event.type == SDL_EVENT_KEY_DOWN){
+        mySound1.ResumeSound();
+        mySound1.PlaySound(false);
+      }
+      if(event.type == SDL_EVENT_KEY_UP){
+        mySound2.ResumeSound();
+        mySound2.PlaySound(false);
+      }
     }
-    // Destroy our renderer
-    SDL_DestroyRenderer(renderer);
-    // Destroy our window
-    SDL_DestroyWindow(window);
+
+    mySound3.ResumeSound();
+    mySound3.PlaySound(true);
+
+    // Update our sprite
+    mySprite.Update();
+
+    // Set the render draw color 
+    SDL_SetRenderDrawColor(renderer,100,190,255,SDL_ALPHA_OPAQUE);
+    // Clear the renderer each time we render
+    SDL_RenderClear(renderer);
+
+    mySprite.Render(renderer);
+
+    // Final step is to present what we have copied into
+    // video memory
+    SDL_RenderPresent(renderer);
+  }
+  // Destroy our renderer
+  SDL_DestroyRenderer(renderer);
+  // Destroy our window
+  SDL_DestroyWindow(window);
 } // end main()
--- dlang/10_audio/sdl3/sdl_audio_example/source/sound.d
+++ dlang/10_audio/sdl3/sdl_audio_example/source/sound.d
@@ -8,60 +8,76 @@ import std.string;
 ///       as the key
 struct Sound{
 
-	string  mFilepath;
-    int 	mID;
+  string  mFilepath;
+  int 	mID;
 
-    // Properties of the Wave File that is loaded
-    SDL_AudioStream* mStream;
-    SDL_AudioSpec mAudioSpec;
-    ubyte*        mWaveData;
-    uint          mWaveDataLength;
+  // Properties of the Wave File that is loaded
+  SDL_AudioStream* mStream;
+  SDL_AudioSpec mAudioSpec;
+  ubyte*        mWaveData;
+  uint          mWaveDataLength;
 
-    // Constructor
-    this(string filepath){
-  		mFilepath = filepath;   
-	   	if(!SDL_LoadWAV(filepath.toStringz,&mAudioSpec, &mWaveData, &mWaveDataLength)){
-            writeln("sound loading error: ",SDL_GetError());
-        }else{
-            writeln("Sound file loaded:",filepath);
-			// Convert .wav to whatever hardware wants
-			mStream = SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &mAudioSpec, null, null);
-        }
+  // Constructor
+  this(string filepath){
+    mFilepath = filepath;   
+    if(!SDL_LoadWAV(filepath.toStringz,&mAudioSpec, &mWaveData, &mWaveDataLength)){
+      writeln("sound loading error: ",SDL_GetError());
+    }else{
+      writeln("Sound file loaded:",filepath);
+      // Convert .wav to whatever hardware wants
+      mStream = SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &mAudioSpec, null, null);
     }
-    // Destructor
-    ~this(){
-//        SDL_FreeWAV(m_waveStart);
-//        SDL_CloseAudioDevice(m_device);
-    }
-	void ResumeSound(){
-		// Sound needs to be 'resumed'. By default, sounds are
-		// otherwise not 'playing'
-		SDL_ResumeAudioStreamDevice(mStream);
-	}
+  }
+  // Destructor
+  ~this(){
+    //        SDL_FreeWAV(m_waveStart);
+    //        SDL_CloseAudioDevice(m_device);
+  }
+  void ResumeSound(){
+    // Sound needs to be 'resumed'. By default, sounds are
+    // otherwise not 'playing'
+    SDL_ResumeAudioStreamDevice(mStream);
+  }
 
-    // PlaySound
-    void PlaySound(){
-        // Queue the audio (so we play when available,
-        //                  as oppososed to a callback function)
-        if(SDL_GetAudioStreamQueued(mStream) < cast(int)mWaveDataLength){
-			SDL_PutAudioStreamData(mStream,mWaveData,mWaveDataLength);	
-		}
+  // PlaySound
+  void PlaySound(bool loop){
+    // Queue the audio (so we play when available,
+    //                  as oppososed to a callback function)
 
+    if(!loop){
+      if(SDL_GetAudioStreamQueued(mStream) < cast(int)mWaveDataLength){
+        SDL_PutAudioStreamData(mStream,mWaveData,mWaveDataLength);	
+        writeln(SDL_GetAudioStreamQueued(mStream));
+      }
     }
-    // Stop the sound
-    void StopSound(){
-//        SDL_PauseAudioDevice(m_device,1);
+    if(loop){
+      // Requeue the sound so that it 'loops'
+      if(SDL_GetAudioStreamQueued(mStream) < cast(int)mWaveDataLength/2){
+        SDL_PutAudioStreamData(mStream,mWaveData,mWaveDataLength);	
+        writeln("currently queued:",SDL_GetAudioStreamQueued(mStream), " < ", cast(int)mWaveDataLength);
+      }
     }
-    // Specific to SDL_Audio API
+  }
+  // Loop sound forever
+  void LoopSound(){
 
-    void SetupDevice(){
-        // Request the most reasonable default device
-        // Set the device for playback for 0, or '1' for recording.
- //       m_device = SDL_OpenAudioDevice(null, 0, &m_audioSpec, null, SDL_AUDIO_ALLOW_ANY_CHANGE);
-        // Error message if no suitable device is found to play
-        // audio on.
-  //      if(0 == m_device){
-  //          writeln("sound device error: ",SDL_GetError()); 
-  //      }
-    }
+  }
+
+
+  // Stop the sound
+  void StopSound(){
+    //        SDL_PauseAudioDevice(m_device,1);
+  }
+
+  // Specific to SDL_Audio API
+  void SetupDevice(){
+    // Request the most reasonable default device
+    // Set the device for playback for 0, or '1' for recording.
+    //       m_device = SDL_OpenAudioDevice(null, 0, &m_audioSpec, null, SDL_AUDIO_ALLOW_ANY_CHANGE);
+    // Error message if no suitable device is found to play
+    // audio on.
+    //      if(0 == m_device){
+    //          writeln("sound device error: ",SDL_GetError()); 
+    //      }
+  }
 }
